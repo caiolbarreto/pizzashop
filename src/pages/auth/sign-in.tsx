e2e0685eed2a0436b486cf320a2aedf355c765cb
Caio Barreto <caiobarreto_28@hotmail.com>
2023-12-29 10:07:15 -0300

    feat: login page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,36 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <h1>Sign in</h1>
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div>
+            <h2 className="text-2xl font-semibold tracking-tight">
+              Access panel
+            </h2>
+            <p className="text-sm text-muted-foreground">
+              Track your sales through the partner panel
+            </p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Your e-mail</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Access panel
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
