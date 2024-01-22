04d1dcd29cbab7af7ffddad9513ff90033ec9a43
Caio Barreto <caiobarreto_28@hotmail.com>
2024-01-22 16:51:45 -0300

    chore: product filters

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -1,5 +1,12 @@
 import { api } from '@/lib/axios'
 
+export interface GetOrdersQuery {
+  pageIndex?: number | null
+  orderId?: string | null
+  customerName?: string | null
+  status?: string | null
+}
+
 export interface GetOrderResponse {
   orders: {
     orderId: string
@@ -15,10 +22,18 @@ export interface GetOrderResponse {
   }
 }
 
-export async function getOrders() {
+export async function getOrders({
+  pageIndex,
+  orderId,
+  customerName,
+  status,
+}: GetOrdersQuery) {
   const response = await api.get<GetOrderResponse>('/orders', {
     params: {
-      pageIndex: 0,
+      pageIndex,
+      orderId,
+      customerName,
+      status,
     },
   })
 
